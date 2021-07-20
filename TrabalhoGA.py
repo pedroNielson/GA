8e402c8632febcfb0bf3dd5bee3b42b54220c534
pedroNielson <pedronielson@gmail.com>
2021-07-20 02:38:37 -0300

    selection of the individuals

--- TrabalhoGA.py
+++ TrabalhoGA.py
@@ -2,10 +2,10 @@ import math
 import random
 
 
-def fitness(population, tam):
+def fitness(pop, tam):
     fitness = []
     for i in range(tam):
-        aux = modal_function(population[i])
+        aux = modal_function(pop[i])
         fitness.append(aux)
     return fitness
 
@@ -38,3 +38,30 @@ def population(tam):
         population.append(aux)
     
     return population
+
+
+def selection(fitness, pop, tam):
+    selection = []
+    for i in range(tam):
+        x1 = random.randint(0,tam)
+        x2 = random.randint(0,tam)
+
+        if fitness[x1] > fitness[x2]:
+            selection.append(pop[x1])
+        elif fitness[x2] > fitness[x1]:
+            selection.append(pop[x2])
+        elif fitness[x1] == fitness[x2]:
+            selection.append(pop[x1])
+    
+    return crossover(selection, tam)
+
+
+def crossover(pop, tam):
+    x=0
+
+
+pop = population(5)
+
+print(pop)
+
+print(fitness(pop,5))
